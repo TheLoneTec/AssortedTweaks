f80d9992ee7d417271c724005e7dc3865432db2f
TheLoneTec <kaceysherrard@hotmail.com>
2025-10-04 21:23:40 +0100

    obsolete method correction (marked for review)

--- Source/AssortedTweaks/CornerBuildable.cs
+++ Source/AssortedTweaks/CornerBuildable.cs
@@ -7,9 +7,9 @@ namespace AssortedTweaks
 {
   internal class CornerBuildable
   {
-    public static bool Prefix(ref bool __result, int cornerX, int cornerZ, PathingContext pc)
+    public static bool Prefix(ref bool __result, IntVec3 adjCardinalX, IntVec3 adjCardinalZ, PathingContext pc)
     {
-      if (!AssortedTweaksMod.instance.Settings.ReplaceStuff_CornerBuildable || !pc.map.thingGrid.ThingsAt(new IntVec3(cornerX, 0, cornerZ)).Any<Thing>((Func<Thing, bool>) (t => TouchPathEndModeUtility.MakesOccupiedCellsAlwaysReachableDiagonally(t.def))))
+      if (!AssortedTweaksMod.instance.Settings.ReplaceStuff_CornerBuildable || !pc.map.thingGrid.ThingsAt(new IntVec3(adjCardinalX.x, 0, adjCardinalZ.z)).Any<Thing>((Func<Thing, bool>) (t => TouchPathEndModeUtility.MakesOccupiedCellsAlwaysReachableDiagonally(t.def))))
         return true;
       __result = true;
       return false;
